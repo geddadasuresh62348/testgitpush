35200d96eda5182ca65fcad62104f2c13afa4dd7
geddadasuresh62348 <lrnothyp@gmail.com>
2023-04-09 11:56:08 +0530

    this is my first commit

---
+++ mongotest1.py
@@ -0,0 +1,15 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://mongo:root@cluster1.7xujqpz.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "id":120,
+    "name":"harshavardhan",
+    "surname":"magala",
+    "email":"gunasekhar@gmail.com"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
